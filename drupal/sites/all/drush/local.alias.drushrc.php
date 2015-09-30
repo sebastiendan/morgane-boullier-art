606828161780ce091695a0879f1e87de35146d83
Sébastien Dan <sebastien@lumini.fr>
2015-09-30 18:35:56 +0200

    drush local alias

---
+++ drupal/sites/all/drush/local.alias.drushrc.php
@@ -0,0 +1,22 @@
+<?php
+
+$options = array(
+	'parent' => '@self',
+	'uri' => 'twinings.local.lumini.fr',
+	'path-aliases' => array(
+		'%files' => 'sites/default/files'
+	),
+	'databases' => array(
+		'default' => array(
+			'default' => array(
+				'database' => 'twinings',
+				'username' => 'twinings',
+				'password' => 'BRNjGVeoIQE3YEA1',
+				'host' => 'localhost',
+				'port' => '',
+				'driver' => 'mysql',
+				'prefix' => '',
+			),
+		),
+	),
+);
