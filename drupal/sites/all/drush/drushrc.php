bad08ea1aabe7f29e8b0b80976c46381a6a0caef
Sébastien Dan <sebastien@lumini.fr>
2015-09-30 13:21:09 +0200

    Drush config.

--- drupal/sites/all/drush/drushrc.php
+++ drupal/sites/all/drush/drushrc.php
@@ -1,8 +1,8 @@
 <?php
 
 $options['shell-aliases'] = array(
-  'pull-files' => '!drush rsync @prod:%files/ @local:%files --mode=rltDv',
-  'pull-db' => '!drush sql-sync @prod @local',
+  'pull-files' => '!drush rsync @dev:%files/ @local:%files --mode=rltDv',
+  'pull-db' => '!drush sql-sync @dev @local',
   'deploy-dev' => '!drush core-rsync @local @dev --exclude-files --delete --mode=rltDv',
 );
 
