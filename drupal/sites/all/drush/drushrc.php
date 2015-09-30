1172e6436c7360c7743cab09230adb0a9ee96a04
andrzej-lumini <andrzej@lumini.fr>
2015-09-30 12:14:32 +0200

    drush

--- drupal/sites/all/drush/drushrc.php
+++ drupal/sites/all/drush/drushrc.php
@@ -2,7 +2,7 @@
 
 $options['shell-aliases'] = array(
   'pull-files' => '!drush rsync @prod:%files/ @local:%files --mode=rltDv',
-  'pull-db' => '!drush sql-sync @prod @local',
+  'pull-db' => '!drush sql-sync @dev @local',
   'deploy-dev' => '!drush core-rsync @local @dev --exclude-files --delete --mode=rltDv',
 );
 
