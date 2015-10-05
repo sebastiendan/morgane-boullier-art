012f0f505e317bbad05c5f5125b7934d21dfefdc
Sébastien Dan <sebastien@lumini.fr>
2015-10-05 12:00:49 +0200

    THeme home.

--- drupal/sites/all/themes/custom/twinings/js/classes/HomeView.class.js
+++ drupal/sites/all/themes/custom/twinings/js/classes/HomeView.class.js
@@ -1,15 +1,19 @@
 function HomeView() {
-
+  this.visibleBannerHeight = 10;
 }
 HomeView._extends(AbstractView);
 
 HomeView.prototype.init = function (tag, parent) {
   HomeView._super.init.call(this, tag, parent);
 
+  this.bind(jQuery(window), 'beforeunload', this.onBeforeUnload);
+
   this.$playButton = this.$tag.find('.video-play-button');
   this.$headline = this.$tag.find('.group-headline');
+  this.$scrollBanner = this.$tag.find('#block-main-home-scroll-banner');
+  this.$groupHeadline = this.$tag.find('.group-headline');
 
-  this.usedHeight = jQuery('.l-page').children('header').height() + 10; // header height + height of scroll banner that is visible
+  this.usedHeight = jQuery('.l-page').children('header').height() + this.visibleBannerHeight;
   this.id = this.$tag.find('.video-js').attr('id');
 
   this.player = videojs(this.id);
@@ -25,16 +29,29 @@ HomeView.prototype.onPlayerReady = function() {
   this.bind(this.$playButton, 'click', this.onPlayButtonClick);
   this.bind(this.$player.find('video'), 'click', this.onVideoClick);
   this.player.on('ended', jQuery.proxy(this.onVideoEnd, this));
+  this.bind(jQuery(window), 'scroll', this.onStageScroll);
   this.bind(jQuery(window), 'resize', this.onStageResize);
 
   this.onStageResize();
 };
 
 HomeView.prototype.onStageResize = function() {
-  var scale = Math.max(jQuery(window).width() / this.initWidth, (jQuery(window).height() - this.usedHeight) / this.initHeight);
+  var ww = jQuery(window).width();
+  var wh = jQuery(window).height();
+
+  var scale = Math.max(ww / this.initWidth, (wh - this.usedHeight) / this.initHeight);
   this.player.width(this.initWidth*scale);
   this.player.height(this.initHeight*scale);
-  this.$player.css({'top':(jQuery(window).height() - this.usedHeight - this.player.height())/2, 'left':(jQuery(window).width() - this.player.width())/2});
+
+  this.$player.css({'top':(wh - this.usedHeight - this.player.height())/2, 'left':(ww - this.player.width())/2});
+  this.$groupHeadline.offset({'top':(wh - this.usedHeight - this.$groupHeadline.height())/1.5});
+
+  this.$scrollBanner.css({'top':wh - this.visibleBannerHeight});
+};
+
+HomeView.prototype.onStageScroll = function() {
+  var wh = jQuery(window).height();
+  this.$scrollBanner.css({'top':wh - this.visibleBannerHeight-jQuery(document).scrollTop()/2});
 };
 
 HomeView.prototype.onPlayButtonClick = function() {
