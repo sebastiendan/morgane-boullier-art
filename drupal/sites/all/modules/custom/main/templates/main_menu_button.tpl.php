7b8d4f5e20e2ff085d9bf9bffce3f6ab28b872b2
Sébastien Dan <sebastien@lumini.fr>
2015-10-14 15:48:15 +0200

    Theme home.

--- drupal/sites/all/modules/custom/main/templates/main_menu_button.tpl.php
+++ drupal/sites/all/modules/custom/main/templates/main_menu_button.tpl.php
@@ -3,5 +3,9 @@
 </div>
 <button>
   <div>Menu</div>
-  <div></div>
+  <svg id="main-menu-button" version="1.1" width="34" height="34" xmlns="http://www.w3.org/2000/svg">
+    <line id="line1" stroke-linecap="round" x1="2" y1="7" x2="32" y2="7" stroke="white" stroke-width="4"/>
+    <line id="line2" stroke-linecap="round" x1="2" y1="17" x2="32" y2="17" stroke="white" stroke-width="4"/>
+    <line id="line3" stroke-linecap="round" x1="2" y1="27" x2="32" y2="27" stroke="white" stroke-width="4"/>
+  </svg>
 </button>
--- drupal/sites/all/themes/custom/twinings/js/classes/Main.class.js
+++ drupal/sites/all/themes/custom/twinings/js/classes/Main.class.js
@@ -2,6 +2,11 @@ function Main() {
 };
 
 Main.prototype.init = function() {
+  if (jQuery('.l-region--navigation').length > 0) {
+    var navigationView = new NavigationView();
+    navigationView.init('.l-region--navigation');
+  }
+
   if (jQuery('body.front').length > 0) {
     var homeView = new HomeView();
     homeView.init('body.front');
