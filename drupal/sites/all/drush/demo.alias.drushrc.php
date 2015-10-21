060a3db2f2f12b11e69de6989ab755e43a66dcaa
Sébastien Dan <sebastien@lumini.fr>
2015-10-21 15:37:14 +0200

    Create demo.

---
+++ drupal/sites/all/drush/demo.alias.drushrc.php
@@ -0,0 +1,38 @@
+<?php
+
+$deploy_to = '/data/www/twinings.demo.lumini.fr/deploy';
+$options = array(
+  'uri' => 'twinings.demo.lumini.fr',
+  'root' => $deploy_to . '/current/drupal',
+  'remote-host' => '10.200.33.9',
+  'remote-user' => 'lumini',
+  'path-aliases' => array(
+    '%files' => 'sites/default/files'
+  ),
+  'databases' => array(
+    'default' => array(
+      'default' => array(
+        'database' => 'twinings',
+        'username' => 'twinings',
+        'password' => 'BRNjGVeoIQE3YEA1',
+        'host' => 'demo.lumini.fr',
+        'port' => '',
+        'driver' => 'mysql',
+        'prefix' => '',
+      ),
+    ),
+  ),
+);
+
+$command_specific = array(
+  'branch' => 'demo',
+  'docroot' => $deploy_to,
+  'deploy-to' => $deploy_to,
+);
+
+$options['command-specific'] = array(
+  'deploy' => $command_specific,
+  'deploy-setup' => $command_specific,
+  'deploy-rollback' => $command_specific,
+  'deploy-cleanup' => $command_specific,
+);
