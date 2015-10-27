352417f3cf2980386001822f12282f610d4d810f
andrzej-lumini <andrzej@lumini.fr>
2015-10-27 17:06:10 +0100

    S3 amazone.

---
+++ drupal/sites/all/modules/custom/main/templates/prehome_cup.tpl.php
@@ -0,0 +1,9 @@
+<div class="prehome prehome-cup fullscreen">
+  <div class="cup" ></div>
+  <div class="texts">
+    <?= $texts; ?>
+  </div>
+  <div class="call">
+    <?= $call; ?>
+  </div>
+</div>
