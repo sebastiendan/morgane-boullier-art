21300aaeea3f34395f55a5f7322b7c829ff48df1
Sébastien Dan <sebastien@lumini.fr>
2015-10-29 19:34:05 +0100

    Stats

--- drupal/sites/all/themes/custom/twinings/js/Stats.js
+++ drupal/sites/all/themes/custom/twinings/js/Stats.js
@@ -16,6 +16,46 @@ Stats.init = function(){
   jQuery('.video-play-button').click(function(){
     Stats.sendEvent('clic_home_video');
   });
+
+  jQuery('.node--home-page .field--name-field-headline .link-wrapper a').click(function(){
+    Stats.sendEvent('clic_home_visuel');
+  });
+
+  jQuery('.field--name-field-tea-image').click(function(){
+    Stats.sendEvent('clic_lecture_typedethé');
+  });
+
+  jQuery('.page-node-6 fieldset.collapsible a').click(function(){
+    Stats.sendEvent('clic_lecture_bienfaits');
+  });
+
+  jQuery('.node--map-country .field--name-title-field').click(function(){
+    Stats.sendEvent('clic_lecture_cartemonde');
+  });
+
+  jQuery('.page-node-9 fieldset.collapsible a').click(function(){
+    Stats.sendEvent('clic_lecture_preparation');
+  });
+
+  jQuery('.page-node-11 fieldset.collapsible a').click(function(){
+    Stats.sendEvent('clic_lecture_teatasters');
+  });
+
+  jQuery('.page-node-12 fieldset.collapsible a').click(function(){
+    Stats.sendEvent('clic_lecture_engagements');
+  });
+
+  jQuery('#bullet-button').click(function(){
+    Stats.sendEvent('clic_lecture_puces');
+  });
+
+  jQuery('input#edit-submit').click(function(){
+    Stats.sendEvent('clic_contact_envoyer');
+  });
+
+  jQuery('#edit-field-category-tid-wrapper label').click(function(){
+    Stats.sendEvent('clic_lecture_gamme');
+  });
 };
 
 Stats.sendEvent = function(eventType, label) {
